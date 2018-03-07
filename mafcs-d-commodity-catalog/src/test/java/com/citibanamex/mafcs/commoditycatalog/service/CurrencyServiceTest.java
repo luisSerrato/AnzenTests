7f0f5e19c18c3a9f8692dff4a1198d6fe9eda7b0
rlerate <rlerate@anzen.com.mx>
2018-03-07 13:07:31 -0600

    Test de Currency finalizado

--- mafcs-d-commodity-catalog/src/test/java/com/citibanamex/mafcs/commoditycatalog/service/CurrencyServiceTest.java
+++ mafcs-d-commodity-catalog/src/test/java/com/citibanamex/mafcs/commoditycatalog/service/CurrencyServiceTest.java
@@ -22,9 +22,14 @@ import com.citibanamex.mafcs.commoditycatalog.formatter.CurrencyFormatter;
 import com.citibanamex.mafcs.commoditycatalog.formatter.SqlStatementRequestFormatter;
 import com.citibanamex.mafcs.commoditycatalog.model.CurrencyContDto;
 import com.citibanamex.mafcs.commoditycatalog.model.CurrencyContinentDto;
+import com.citibanamex.mafcs.commoditycatalog.model.CurrencyDto;
 import com.citibanamex.mafcs.commoditycatalog.service.impl.CurrencyContinentServiceImpl;
+import com.citibanamex.mafcs.commoditycatalog.service.impl.CurrencyServiceImpl;
+import com.citibanamex.mafcs.commoditycatalog.service.impl.CurrencySummaryServiceImpl;
 import com.citibanamex.mafcs.commoditycatalog.util.Util;
 import com.citibanamex.mafcs.commoditycatalog.viewmodel.currencycontinent.ContinentCurrencyResponse;
+import com.citibanamex.mafcs.commoditycatalog.viewmodel.currencydetail.CurrencyResponse;
+import com.citibanamex.mafcs.commoditycatalog.viewmodel.currencysummary.CurrencySummaryResponse;
 import com.citibanamex.mafcs.commoditycatalog.viewmodel.query.SqlStatementResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -43,6 +48,16 @@ public class CurrencyServiceTest {
 		}
 
 		@Bean
+		public CurrencySummaryService currencySummaryService() {
+			return new CurrencySummaryServiceImpl();
+		}
+
+		@Bean
+		CurrencyService currencyService() {
+			return new CurrencyServiceImpl();
+		}
+
+		@Bean
 		public SqlStatementRequestFormatter sqlStatementRequestFormatter() {
 			return new SqlStatementRequestFormatter();
 		}
@@ -57,10 +72,18 @@ public class CurrencyServiceTest {
 	@Autowired
 	private CurrencyContinentService currencyContinentService;
 
+	@Autowired
+	private CurrencySummaryService currencySummaryService;
+
+	@Autowired
+	private CurrencyService currencyService;
+
 	@MockBean
 	private DatabaseMsClient databaseMsClient;
 
 	private SqlStatementResponse resultSet;
+
+	private SqlStatementResponse resultSetCurrency;
 
 	@Autowired
 	private CurrencyFormatter currencyFormatter;
@@ -70,20 +93,24 @@ public class CurrencyServiceTest {
 	private List<CurrencyContDto> currencies;
 	private List<CurrencyContinentDto> lstCurrencyContinent;
 
+	private List<CurrencyDto> lstCurrencyDto;
+
 	@Before
 	public void setUp() {
+
+		// CurrencyContinent
 
 		resultSet = new SqlStatementResponse();
 		resultSet.setResultSet(null);
 		lstObj = new ArrayList<>();
 
 		Object[] objAl = new Object[8];
-		objAl[0] = "currencyKey";
-		objAl[1] = "countryFlag";
-		objAl[2] = "countryNameSp";
-		objAl[3] = "countryNameEn";
-		objAl[4] = "currencyDescSp";
-		objAl[5] = "currencyDescEn";
+		objAl[0] = "1";
+		objAl[1] = "argentina.png";
+		objAl[2] = "Argentina";
+		objAl[3] = "Argentina";
+		objAl[4] = "Peso";
+		objAl[5] = "Peso";
 		objAl[6] = 6;
 		objAl[7] = 7;
 
@@ -117,6 +144,37 @@ public class CurrencyServiceTest {
 		lstCurrencyContinent = new ArrayList<>();
 		lstCurrencyContinent.add(currencyContinentDto);
 
+		// Currency Service
+
+		Object[] objCurrencyService = new Object[8];
+		objCurrencyService[0] = "1.768753573";
+		objCurrencyService[1] = "Canada";
+		objCurrencyService[2] = "Canada";
+		objCurrencyService[3] = "Dollar";
+		objCurrencyService[4] = "Dollar";
+		objCurrencyService[5] = 1.7683;
+
+		resultSetCurrency = new SqlStatementResponse();
+		resultSetCurrency.setResultSet(null);
+		lstObj = new ArrayList<>();
+
+		lstObj.add(objCurrencyService);
+
+		resultSetCurrency.setResultSet(lstObj);
+
+		// CurrencyService Formatter
+
+		CurrencyDto currencyDto = new CurrencyDto();
+		currencyDto.setCurrencyKey("1.768753573");
+		currencyDto.setCountryNameSp("Canada");
+		currencyDto.setCountryNameEn("Canada");
+		currencyDto.setCurrencyDescSp("Dollar");
+		currencyDto.setCurrencyDescEn("Dollar");
+		currencyDto.setSalePrice(1.7683);
+
+		lstCurrencyDto = new ArrayList<>();
+		lstCurrencyDto.add(currencyDto);
+
 	}
 
 	@Test
@@ -127,7 +185,7 @@ public class CurrencyServiceTest {
 		ObjectMapper mapper = new ObjectMapper();
 		String strMapper = mapper.writeValueAsString(lstCurrencyContinent);
 
-		logger.info("Response " + strMapper);
+		logger.info("Response testCurrencyContinentService" + strMapper);
 	}
 
 	@Test
@@ -137,7 +195,50 @@ public class CurrencyServiceTest {
 		ObjectMapper mapper = new ObjectMapper();
 		String strMapper = mapper.writeValueAsString(response);
 
-		logger.info("Response formatter Continent"+ strMapper);
+		logger.info("Response Continent formatter" + strMapper);
+
+	}
+
+	@Test
+	public void testCurrencySummaryService() throws Exception {
+
+		List<CurrencyContDto> lstCurrencySummary = currencySummaryService.fetchCurrencyByKey(Util.getSummKeyCurrency());
+		ObjectMapper mapper = new ObjectMapper();
+		String strMapper = mapper.writeValueAsString(lstCurrencySummary);
+
+		logger.info("Response summary " + strMapper);
+	}
+
+	@Test
+	public void testCurrencySummaryFormatter() throws Exception {
+
+		CurrencySummaryResponse response = currencyFormatter.currencySummaryResponse(currencies);
+		ObjectMapper mapper = new ObjectMapper();
+		String strMapper = mapper.writeValueAsString(response);
+
+		logger.info("Response summary formatter" + strMapper);
+
+	}
+
+	@Test
+	public void testCurrencyService() throws Exception {
+
+		Mockito.when(databaseMsClient.query(Mockito.any())).thenReturn(resultSetCurrency);
+		List<CurrencyDto> lstCurrency = currencyService.fetchCurrencyByNotKey(Util.getCalcKeyCurrency());
+		ObjectMapper mapper = new ObjectMapper();
+		String strMapper = mapper.writeValueAsString(lstCurrency);
+
+		logger.info("Response test CurrencyService" + strMapper);
+	}
+
+	@Test
+	public void testCurrencyServiceFormatter() throws Exception {
+
+		CurrencyResponse response = currencyFormatter.currencyResponse(lstCurrencyDto);
+		ObjectMapper mapper = new ObjectMapper();
+		String strMapper = mapper.writeValueAsString(response);
+
+		logger.info("Response CurrencyService formatter" + strMapper);
 
 	}
 
